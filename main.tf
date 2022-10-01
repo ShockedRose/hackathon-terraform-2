337511fdd7a6b5455a927a058554f57a500a5187
Miguel <miguelj0916@gmail.com>
2022-10-01 15:35:39 -0500

    faulty finished (os disk shared problem)

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = var.resource_group_name
+  tags = var.tags
 }
 
 resource "azurerm_virtual_network" "my_terraform_network" {
@@ -8,6 +9,7 @@ resource "azurerm_virtual_network" "my_terraform_network" {
   address_space       = var.network_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  tags = var.tags
 }
 
 # Create subnet
@@ -24,86 +26,83 @@ resource "azurerm_public_ip" "linux_VM_virtual_machine_IP" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
+  tags = var.tags
 }
 resource "azurerm_public_ip" "windows_VM_virtual_machine_IP" {
   name                = var.windows_ip_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
+  tags = var.tags
 }
-
-# Create Network Security Group and rule
 resource "azurerm_network_security_group" "my_terraform_nsg" {
   name                = "myNetworkSecurityGroup"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "SSH"
+    name                       = "SSH HTTP"
     priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "22"
+    destination_port_range     = "22-80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  tags = var.tags
 }
 
-# Create network interface
-resource "azurerm_network_interface" "my_terraform_nic" {
-  name                = "myNIC"
+resource "azurerm_network_interface" "my_terraform_linux_nic" {
+  name                = var.linux_ip_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "my_nic_configuration"
+    name                          = "my_Linux_nic_configuration"
     subnet_id                     = azurerm_subnet.my_terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.linux_VM_virtual_machine_IP.id
   }
+  tags = var.tags
 }
+resource "azurerm_network_interface" "my_terraform_windows_nic" {
+  name                = var.windows_ip_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-# Connect the security group to the network interface
+  ip_configuration {
+    name                          = "my_Windows_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.windows_VM_virtual_machine_IP.id
+  }
+  tags = var.tags
+}
 resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_interface_id      = azurerm_network_interface.my_terraform_linux_nic.id
   network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
 }
-
-# Generate random text for a unique storage account name
 resource "random_id" "random_id" {
   keepers = {
-    # Generate a new ID only when a new resource group is defined
     resource_group = azurerm_resource_group.rg.name
   }
 
   byte_length = 8
 }
-
-# Create storage account for boot diagnostics
-resource "azurerm_storage_account" "my_storage_account" {
-  name                     = "diag${random_id.random_id.hex}"
-  location                 = azurerm_resource_group.rg.location
-  resource_group_name      = azurerm_resource_group.rg.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-# Create (and display) an SSH key
 resource "tls_private_key" "example_ssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-# Create virtual machine
-resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
-  name                  = "myVM"
+resource "azurerm_linux_virtual_machine" "my_linux_terraform_vm" {
+  name                  = var.linux_VM_name
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  size                  = "Standard_DS1_v2"
-
+  network_interface_ids = [azurerm_network_interface.my_terraform_linux_nic.id]
+  size                  = "Standard_B2s"
+  tags = var.tags
   os_disk {
     name                 = "myOsDisk"
     caching              = "ReadWrite"
@@ -112,21 +111,42 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
     version   = "latest"
-  }
+    }
 
   computer_name                   = "myvm"
   admin_username                  = "azureuser"
   disable_password_authentication = true
-
   admin_ssh_key {
     username   = "azureuser"
     public_key = tls_private_key.example_ssh.public_key_openssh
   }
 
-  boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
+}
+resource "azurerm_windows_virtual_machine" "my_windows_terraform_vm" {
+  name                  = var.windows_VM_name
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_windows_nic.id]
+  size                  = "Standard_B2s"
+  tags = var.tags
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
   }
+
+  source_image_reference {
+     publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+    }
+
+  computer_name                   = "myvm"
+  admin_username                  = "azureuser"
+  admin_password = "8Digitos*"
+
 }
